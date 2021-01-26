b5725fe225ef4ec78715b350a3d9af98fc625f73
Pedro Contine <pedro.contine@gmail.com>
2021-01-26 08:27:04 -0300

    Initial Commit

---
+++ ShareKit/ShareKit.h
@@ -0,0 +1,18 @@
+//
+//  ShareKit.h
+//  ShareKit
+//
+//  Created by Pedro Contine on 26/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ShareKit.
+FOUNDATION_EXPORT double ShareKitVersionNumber;
+
+//! Project version string for ShareKit.
+FOUNDATION_EXPORT const unsigned char ShareKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShareKit/PublicHeader.h>
+
+
